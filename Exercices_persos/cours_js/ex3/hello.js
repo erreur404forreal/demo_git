2ef07d1a1cad19c255b537560433c8c9124dd918
Josue CEREME <ceremej@gmail.com>
2018-09-02 23:44:35 +0200

    mygitrepo

---
+++ Exercices_persos/cours_js/ex3/hello.js
@@ -0,0 +1,16 @@
+var i = 1;
+while(i < 10)
+{
+  alert(i);
+  i++;
+}
+
+for(int a=0; a<10; a++)
+{
+  alert(a);
+}
+
+var nom;
+do
+  nom = prompt('Entrez un nom');
+while(saisie != null && saisie !='');
